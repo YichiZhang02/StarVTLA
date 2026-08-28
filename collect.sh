e29a06e44b6002602cdec779099296c6dbf7f064
YichiZhang02 <zyc20020502@gmail.com>
2026-08-28 18:37:05 +0800

    ...

--- collect.sh
+++ collect.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")"   # 切到仓库根, 使 playground/... 相对路径生效,
 # =================== 可调参数 ===================
 robot_type=rm_isf_umi_left                    # rm_base_umi_dual | rm_isf_umi_left
 
-name=${1:-insert_easy_robust3}                # 数据集基础名
-single_task=${2:-"insert the object to the hole"}        # 任务文字描述 (会写入每一帧)
-num_episodes=${3:-25}                      # 录制集数
+name=${1:-insert_usb}                # 数据集基础名
+single_task=${2:-"insert the usb to the hole"}        # 任务文字描述 (会写入每一帧)
+num_episodes=${3:-100}                      # 录制集数
 mode=${4:-drag}                          # teleop | drag
-drag_gripper_close_value=${5:-0.3}         # 0=最紧, 1=全开
+drag_gripper_close_value=${5:-0.0}         # 0=最紧, 1=全开
 
 # 复位选项
 reset_before_episode=${6:-true}           # true=按左右键结束时先复位确认，再保存或重录
@@ -22,7 +22,7 @@ repo_id="local/${robot_type}_$(date +%Y%m%d)_${name}"
 
 
 fps=30
-episode_time_s=60                                # 每集最长录制秒数 (可中途按右键提前保存)
+episode_time_s=300                                # 每集最长录制秒数 (可中途按右键提前保存)
 # 不传 --dataset.root: collect.py 默认存到 playground/data/<repo_id 末段> (相对路径)
 
 # =================== 启动 ===================
